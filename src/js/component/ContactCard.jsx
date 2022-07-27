7d719caf5bbb5eb8ee6f96dff09fa5a6150f1a19
MikeGajardo <mikegajardo@gmail.com>
2022-07-27 03:10:52 +0000

    updated styles

--- src/js/component/ContactCard.jsx
+++ src/js/component/ContactCard.jsx
@@ -8,20 +8,26 @@ export const ContactCard = (props) => {
     <div className="card contact-card m-auto p-3">
       <div className="details d-flex">
         <img
-          className="profile-pic ms-3"
+          className="profile-pic mx-3"
           src="https://external-preview.redd.it/Tz6nNKdrp6UlZhO26SIFcl8Tl1aH_eF7YLYK7A7N3RU.jpg?auto=webp&s=a9189138061b013f03beefdc3111d4dc97d97a1a"
         ></img>
         <div className="contact-text">
-          <h4>{props.fullname}</h4>
-          <i className="fas fa-map-marker-alt text-muted mr-3" />
+          <h4 className="mx-2 p-1">{props.fullname}</h4>
+          <div className="d-flex">
+          <i className="fas fa-map-marker-alt text-muted mx-2 p-1" />
           <p>{props.address}</p>
-          <span className="fa fa-phone fa-fw text-muted mr-3" />
-          <p>{props.phone}</p>
-          <span className="fa fa-envelope fa-fw text-muted mr-3" />
+          </div>
+          <div className="d-flex">
+          <span className="fa fa-phone fa-fw text-muted mx-2 p-1" />
+          <p>{props.phone}</p><br></br>
+          </div>
+          <div className="d-flex">
+          <span className="fa fa-envelope fa-fw text-muted mx-2 p-1" />
           <p>{props.email}</p>
+          </div>
         </div>
         <div className="card-icons">
-          <button onClick={() => actions.deleteContact(props.id)}>
+          <button className="mx-2" onClick={() => actions.deleteContact(props.id)}>
             <i className="fas fa-trash-alt btn" aria-hidden="true"></i>
           </button>
           <Link to={`/edit-contact/${props.id}`}>
